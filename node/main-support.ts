c55fb623e6c9e07a108acb649b791103d9b45804
Jozef Kopčák <jozef.kopcak@student.tuke.sk>
2021-12-15 13:03:11 +0100

    test4

--- node/main-support.ts
+++ node/main-support.ts
@@ -353,7 +353,7 @@ function getBitrate(fileSize, duration) {
 function computeNumberOfScreenshots(screenshotSettings: ScreenshotSettings, duration: number): number {
   let total: number;
 
-  // fixed or per minute  s
+  // fixed or per minutes
   if (screenshotSettings.fixed) {
     total = screenshotSettings.n;
   } else {
